e084317924474cf0ff7f7c42d0da5ec9a908b9bb
MosaadIbrahimB <mosaadibrahim85@gmail.com>
2024-10-05 05:22:08 +0300

    add test favorite screen

--- test/favorite_screen.dart
+++ test/favorite_screen.dart
@@ -4,7 +4,7 @@ import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 import 'package:provider/provider.dart';
 import 'package:untitled13/provider/favorites.dart';
-import 'package:untitled13/screens/home.dart';
+import 'package:untitled13/screens/favorites.dart';
 
 import 'favorite_moc.mocks.dart';
 
@@ -12,23 +12,23 @@ import 'favorite_moc.mocks.dart';
 void main() {
   late MockFavoritesProvider mockProvider;
   group(
-    'Test home screen',
-    () {
-      setUp(
+    'Test favorite screen',
         () {
+      setUp(
+            () {
           mockProvider = MockFavoritesProvider();
         },
       );
 
       testWidgets(
         'list view display item',
-        (widgetTester) async {
+            (widgetTester) async {
           when(mockProvider.items).thenReturn([1, 2, 3, 4]);
           await widgetTester
               .pumpWidget(ChangeNotifierProvider<FavoritesProvider>.value(
             value: mockProvider,
             child: const MaterialApp(
-              home: HomePage(),
+              home: FavoritesPage(),
             ),
           ));
           expect(find.text('Item 1'), findsOneWidget);
@@ -37,26 +37,27 @@ void main() {
           expect(find.text('Item 4'), findsOneWidget);
         },
       );
+
+
+
+
       testWidgets(
         'Favorite icon pressed',
-        (widgetTester) async {
-          when(mockProvider.items).thenReturn([]);
+            (widgetTester) async {
+          when(mockProvider.items).thenReturn([5]);
           await widgetTester.pumpWidget(
             ChangeNotifierProvider<FavoritesProvider>.value(
               value: mockProvider,
               child: const MaterialApp(
-                home: HomePage(),
+                home: FavoritesPage(),
               ),
             ),
           );
 
-          await widgetTester.tap(find.byIcon(Icons.favorite_border).first);
-          verify(mockProvider.add(any)).called(1);
+          await widgetTester.tap(find.byIcon(Icons.favorite));
+          verify(mockProvider.remove(5)).called(1);
         },
       );
-
-
-
     },
   );
 }
